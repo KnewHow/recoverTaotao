a6869bcc80b34b1c2ce91e4c97070f9ffda95dc8
yghtest <948170910@qq.com>
2017-08-27 18:49:38 +0800

    commit

---
+++ ygh-test/src/main/java/test/Demo.java
@@ -0,0 +1,14 @@
+package test;
+/**
+ * 
+ * @author yuangh
+ *
+ * @company erongdu
+ *
+ * @data 2017年8月27日
+ */
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+}	
